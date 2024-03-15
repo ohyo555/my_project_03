927bcc0eb30d29aea931391660f34d48e697749a
ohyo555 <555gywn@gamil.com>
2024-03-15 10:35:25 +0900

    크롤링 하는중

--- src/main/java/com/example/demo/controller/UsrCrawlingController.java
+++ src/main/java/com/example/demo/controller/UsrCrawlingController.java
@@ -9,16 +9,15 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Controller;
 
@@ -117,13 +116,12 @@ public class UsrCrawlingController {
 			        List<WebElement> elements = driver.findElements(By.cssSelector("tr"));
 
 			        // CSV 파일에 데이터 쓰기
-		            saveToCSV(elements, "output.csv");
-		            saveToFile(elements, "rs.txt");
-		            saveToCSV2(elements, "123.csv");
+		            //saveToCSV(elements, "output.csv");
+		            saveToFile(elements, "result.txt");
 		            
 			        for (WebElement element : elements) {
 		                String text = element.getText();
-		                System.out.println(text);
+		                //System.out.println(text);
 		            }
 
 			    } catch (Exception e) {
@@ -140,17 +138,7 @@ public class UsrCrawlingController {
             // CSV 헤더 작성 (필요에 따라 수정)
         	writer.write("\uFEFF");
             writer.write("날짜,순번,구분,경기,시간,구장,중계일정,라운드,비고\n");
-//
-//            for(int i = 1; i < elements.size(); i++) {
-//        		for(int j = 1; j <= 9; j++) {
-//        			WebElement title = elements.get(i);
-//        			WebElement text = title.findElement(By.xpath("//*[@id=\"root\"]/article/div/article/section/article/section[2]/article/section[2]/table/tbody/tr[" + i + "]/td[" + j + "]"));
-//        			String t = text.getText().replace("\n", ",");
-//                	writer.write(t);
-//                    writer.newLine();
-//        		}	
-//        	};
-//        	
+
         	for(int i = 1; i < elements.size(); i++) {
         		for(int j = 1; j <= 9; j++) {
         			WebElement title = elements.get(i);
@@ -164,21 +152,7 @@ public class UsrCrawlingController {
                 	writer.write(t);
         		}	
         	};
-//        	for (int i = 1; i <= 37; i++) { // Iterate through each row (tr)
-//        	    WebElement row = driver.findElement(By.xpath("//*[@id=\"root\"]/article/div/article/section/article/section[2]/article/section[2]/table/tbody/tr[" + i + "]"));
-//        	    WebElement cell = row.findElement(By.xpath("./td[9]")); // Get the 9th column (td[9]) within the current row
-//        	    String text = cell.getText();
-//        	    writer.write(text + "\n");
-//        	}
-//        	
-        	
-//            for (WebElement element : elements) {
-//                // 가져온 데이터를 CSV 파일에 작성 (필요에 따라 수정)
-//            	//String title = element.findElement(By.cssSelector("td")).getText();
-//            	String text = element.getText().replace("\n", ",");
-//                writer.write(text);
-//                writer.newLine();
-//            }
+
             System.out.println("csv 파일 저장!!!");
         }
     }
@@ -188,74 +162,64 @@ public class UsrCrawlingController {
         System.out.println(elements.size());
 
         	for(int i = 1; i < elements.size(); i++) {
+        		
+        		List<String> resultData = new ArrayList<>();
+        		
         		for(int j = 1; j <= 9; j++) {
         			WebElement title = elements.get(i);
         			WebElement text = title.findElement(By.xpath("//*[@id=\"root\"]/article/div/article/section/article/section[2]/article/section[2]/table/tbody/tr[" + i + "]/td[" + j + "]"));
-        			String t = text.getText().replace("\n", ",").replace(")","),").replace("여자", ",여자,").replace("19", ",19").replace("16", ",16").replace(":00",":00,").replace(":30",":30,").replace("육관","육관,").replace("움","움,").replace("ER","ER,").replace("nd","nd,");
-        			String a = t.replace(".,", ".").replace(",,", ",");
-        			if (a.contains("(")) { // "결과"라는 문자열이 포함된 경우
+        			String t = text.getText().replace("\n", ",").replace(")","),").replace("여자", ",여자,").replace("19", ",19").replace("16", ",16").replace(":00",":00,").replace(":30",":30,");
+        			t = t.replace("육관","육관,").replace("움","움,").replace("ER","ER,").replace("nd","nd,");
+        			t = t.replace("트,상", "트 상").replace("트,전", "트 전").replace("교,하", "교 하");
+        			t = t.replace(".,", ".").replace(" , ", " ");
+        			// t = t.replace("),,", "),");
+
+        			if (t.contains("(")) { // "결과"라는 문자열이 포함된 경우
                         writer.write("\n"); // 새로운 행으로 이동
                     }
-        			
-                	writer.write(a);
+
+                	writer.write(t);
         		}	
         	};
         
         writer.close();
         System.out.println("데이터를 파일에 저장했습니다.");
-        
-        String inputFilePath = "rs.txt";
-        String outputFilePath = "111111111.csv";
-        String delimiter = ","; // 텍스트 파일에서 사용하는 구분자 설정
-        
-        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
-             BufferedWriter writer2 = new BufferedWriter(new FileWriter(outputFilePath))) {
-            String line;
-            
-            writer2.write("\uFEFF");
-            
-            while ((line = reader.readLine()) != null) {
-                String[] fields = line.split(delimiter); // 구분자를 기준으로 줄을 분할
-                for (int i = 0; i < fields.length; i++) {
-                    writer2.write(fields[i]);
-                    if (i < fields.length - 1) {
-                        writer2.write(","); // 필드 사이에 쉼표 추가
-                    }
-                }
-                writer2.newLine(); // CSV 파일에서 다음 줄로 이동
-            }
-            System.out.println("텍스트 파일이 성공적으로 CSV로 변환되었습니다!");
-        } catch (IOException e) {
-            System.err.println("오류: " + e.getMessage());
-        }
+      
     }
-	
-	private void saveToCSV2(List<WebElement> elements, String filePath) throws IOException {
-        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filePath), StandardCharsets.UTF_8))) {
-            // CSV 헤더 작성 (필요에 따라 수정)
-        	writer.write("\uFEFF");
-            writer.write("날짜,순번,구분,경기,시간,구장,중계일정,라운드,비고\n");
-
-        for(int i = 1; i < elements.size(); i++) {
-    		for(int j = 1; j <= 9; j++) {
-    			WebElement title = elements.get(i);
-    			WebElement text = title.findElement(By.xpath("//*[@id=\"root\"]/article/div/article/section/article/section[2]/article/section[2]/table/tbody/tr[" + i + "]/td[" + j + "]"));
-//    			String a = text.getText() + ",";
-    			String t = text.getText().replace("\n", ",").replace(" (", ",(").replace(")","),").replace("여자", "여자,").replace(":00",":00,").replace(":30",":30,").replace("육관","육관,").replace("움","움,").replace("ER","ER,").replace("nd","nd,");
-            	
-    			writer.write(t);
-                writer.newLine();
-    		}	
-    	};
-    	
-        writer.close();
-        System.out.println("데이터를 파일에 저장했습니다.");
-    }}
-	
 	
 	public static void main(String[] args) {
     	UsrCrawlingController webCrawler = new UsrCrawlingController();
-        webCrawler.crawl2();
+        // webCrawler.crawl2();
+    	
+    	// txt -> csv
+    	String textFilePath = "result.txt"; 
+        String csvFilePath = "result.csv";
+
+        try {
+            convertTextToCSV(textFilePath, csvFilePath);
+            System.out.println("Conversion completed successfully.");
+        } catch (IOException e) {
+            System.err.println("Error occurred during conversion: " + e.getMessage());
+        }
+    }
+
+    public static void convertTextToCSV(String textFilePath, String csvFilePath) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(textFilePath));
+        FileWriter writer = new FileWriter(csvFilePath);
+        writer.write("\uFEFF");
+        String line;
+        while ((line = reader.readLine()) != null) {
+        	// 쉼표를 기준으로 데이터를 분리
+        	String[] data = line.split(",");
+        	// 각 데이터를 다음 행의 새 셀에 쓰기
+            for (String datum : data) {
+                writer.write(datum.trim() + ",");
+            }
+            writer.write("\n");
+        }
+
+        reader.close();
+        writer.close();
+    	
     }
 }
-	
